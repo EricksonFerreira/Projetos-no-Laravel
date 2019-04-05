33453b3633908212cda3c93725df346569c47abd
Erickson <erickson.rinho@hotmail.com>
2019-04-05 11:51:45 -0300

    Atividade de Alexandre

--- blog/database/migrations/2019_03_22_185430_create_shares_table.php
+++ blog/database/migrations/2019_03_22_185430_create_shares_table.php
@@ -9,12 +9,62 @@ class CreateSharesTable extends Migration
     public function up()
     {
         Schema::create('shares', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->autoIncrement();
             $table->string('share_name');
             $table->integer('share_price');
-            $table->integer('share_qty');
+            $table->binary('data');
+            $table->boolean('confirmed');
+            $table->char('name', 100)->after('column');
+            $table->date('created_at');
+            $table->dateTime('created_at');
+            $table->double('amount', 8, 2)
+            $table->enum('level', ['easy', 'hard'])->first();
+            $table->float('amount', 8, 2)->nullable($value);
+            $table->geometry('positions');
+            $table->geometryCollection('positions')->always();
+            $table->increments('id');
+            $table->integer('votes')->nullable($value);
+            $table->ipAddress('visitor');
+            $table->json('options')->useCurrent();
+            $table->lineString('positions');
+            $table->longText('description');
+            $table->macAddress('device');
+            $table->mediumIncrements('id')->useCurrent();
+            $table->mediumInteger('votes');
+            $table->mediumText('description');
+            $table->morphs('taggable');
+            $table->multiLineString('positions');
+            $table->multiPoint('positions');
+            $table->multiPolygon('positions');
+            $table->nullableMorphs('taggable')->generatedAs($expressio);
+            $table->nullableTimestamps();
+            $table->point('position');
+            $table->polygon('positions');
+            $table->rememberToken();
+            $table->smallIncrements('id');
+            $table->smallInteger('votes');
+            $table->softDeletes();
+            $table->softDeletesTz();
+            $table->string('name', 100);
+            $table->text('description')->comment('my');
+            $table->time('sunrise');
+            $table->timeTz('sunrise');
+            $table->timestamp('added_on');
+            $table->timestampTz('added_on');
             $table->timestamps();
-        });
+            $table->timestampsTz();
+            $table->tinyIncrements('id');
+            $table->tinyInteger('votes');
+            $table->unsignedBigInteger('votes');
+            $table->unsignedDecimal('amount', 8, 2)->collation('utf8_unicode_ci');
+            $table->unsignedInteger('votes');
+            $table->unsignedMediumInteger('votes')->charset('utf8');
+            $table->unsignedSmallInteger('votes');
+            $table->unsignedTinyInteger('votes');
+            $table->uuid('id');
+            $table->year('birth_year');
+
+
     }
 
     public function down()
